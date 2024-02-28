b7a954aad63f182f2ef754ac946669be04fc85fe
VadGov <148807136+VadGov@users.noreply.github.com>
2024-02-28 18:06:00 +0200

    create e2e

--- e2e/test.py
+++ e2e/test.py
@@ -1,19 +1,60 @@
-import re
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Playwright, sync_playwright, expect
 
 
-def test_has_title(page: Page):
-    page.goto("https://playwright.dev/")
+def test_e2e_begin(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://sportshop.com.ua/")
 
-    # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile('Playwright'))
+    # Локалізація
+    page.get_by_role("button", name="Українська").click()
+    page.get_by_text("Русский").click()
+    expect(page.get_by_text("СПОРТИВНОЕ ПИТАНИЕ")).to_be_visible()
+    page.get_by_role("button", name="Русский").click()
+    page.get_by_text("Українська").click()
+    expect(page.get_by_text("СПОРТИВНЕ ХАРЧУВАННЯ")).to_be_visible()
+
+    # Авторизація
+    page.get_by_role("button", name="Мій аккаунт").click()
+    page.get_by_text("Авторизація / Реєстрація").click()
+    page.get_by_placeholder("Електронна пошта").fill("vadvadonik@gmail.com")
+    page.get_by_placeholder("Пароль").fill("175935")
+    page.get_by_role("button", name="Увійти").click()
+
+    # пошук
+    page.locator("(//input[@placeholder='Що будемо шукати'])[1]").fill("mutant")
+    page.locator("div[class='col-hidden col-md-visible col-xl-4 pull-xl-3'] button[class='search__btn']").click()
+
+    # фильтр по Сироватковий протїн
+    page.locator("(//div[@class='jq-selectbox__select-text'])[1]").click()
+    page.locator("li:nth-child(60)").click()
+    page.get_by_role("button", name="Пошук").click()
+
+    # сортировка
+    page.get_by_role("button", name="Сортування: За замовчуванням").click()
+    page.locator("div[class='options options--desktop'] li:nth-child(6)").click()
+
+    # клик по карточке
+    page.locator("(//img[@alt='Mutant Whey Protein 2270 g'])[1]").click()
+    page.locator("//button[@id='button-cart']").click()
+
+    # замовити
+    page.locator("a[class='btn btn--blue']").click()
+
+    page.pause()
+    # зворотній зв'язок
+    page.locator("//a[@class='nav__link'][contains(text(),'Контакти')]").click()
+    page.locator('[class="link link--dashed"]').click()
+    page.locator("div[id='contact-form'] button[class='popup__close']").click()
+
+    # Вихід з ос каб
+    page.locator('[class="acc__btn js-toggle-btn"]').click()
+    page.get_by_text("Вихід").click()
+
+    context.close()
+    browser.close()
 
 
-def test_get_started_link(page: Page):
-    page.goto("https://playwright.dev/")
-
-    # Click the get started link.
-    page.get_by_role("link", name="Get started").click()
-
-    # Expects page to have a heading with the name of Installation.
-    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+with sync_playwright() as playwright:
+    test_e2e_begin(playwright)
