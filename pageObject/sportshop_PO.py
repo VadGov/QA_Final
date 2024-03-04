bbbf4c389629c080e48d32c656b521fb68bdb13c
VadGov <148807136+VadGov@users.noreply.github.com>
2024-03-04 20:18:01 +0200

    rename def

--- pageObject/sportshop_PO.py
+++ pageObject/sportshop_PO.py
@@ -10,7 +10,7 @@ class SportShop:
     def open_url(self, url: str):
         self.page.goto(url)
 
-    def localisation(self):
+    def switch_language_rus_ua(self):
         self.page.get_by_role("button", name="Українська").click()
         self.page.get_by_text("Русский").click()
         # self.expect(page.get_by_text("СПОРТИВНОЕ ПИТАНИЕ")).to_be_visible()
@@ -25,21 +25,21 @@ class SportShop:
         self.page.get_by_placeholder("Пароль").fill("v123456789d")
         self.page.get_by_role("button", name="Увійти").click()
 
-    def search(self):
+    def search_product(self):
         self.page.locator("(//input[@placeholder='Що будемо шукати'])[1]").fill("mutant")
         self.page.locator(
             "div[class='col-hidden col-md-visible col-xl-4 pull-xl-3'] button[class='search__btn']").click()
 
-    def filtr(self):
+    def filtr_whey_protein(self):
         self.page.locator("(//div[@class='jq-selectbox__select-text'])[1]").click()
         self.page.locator("li:nth-child(60)").click()
         self.page.get_by_role("button", name="Пошук").click()
 
-    def sort(self):
+    def sort_by_high_rating(self):
         self.page.get_by_role("button", name="Сортування: За замовчуванням").click()
         self.page.locator("div[class='options options--desktop'] li:nth-child(6)").click()
 
-    def purchase_selection(self):
+    def purchase_selected_product(self):
         self.page.locator("(//img[@alt='Mutant Whey Protein 2270 g'])[1]").click()
         self.page.locator("//button[@id='button-cart']").click()
 
@@ -56,7 +56,7 @@ class SportShop:
         self.page.locator('[class="link link--dashed"]').click()
         self.page.locator("div[id='contact-form'] button[class='popup__close']").click()
 
-    def exit_acc(self):
+    def exit_account(self):
         self.page.locator('[class="acc__btn js-toggle-btn"]').click()
         self.page.get_by_text("Вихід").click()
 
