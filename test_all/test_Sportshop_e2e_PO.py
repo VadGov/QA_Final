045154ef65518f5b2f066a0dbab7eef2e4ef4325
VadGov <148807136+VadGov@users.noreply.github.com>
2024-03-16 11:51:22 +0200

    Update test_Sportshop_e2e_PO.py
      repare name

--- test_all/test_Sportshop_e2e_PO.py
+++ test_all/test_Sportshop_e2e_PO.py
@@ -2,9 +2,9 @@
 def test_sportshop_e2e(desktop_sportshop):
     desktop_sportshop.open_url("https://sportshop.com.ua/")
     desktop_sportshop.change_lang_ua_to_rus_and_back()
-    desktop_sportshop.autorithation()
+    desktop_sportshop.authorization()
     desktop_sportshop.search_product()
-    desktop_sportshop.choice_whey_protein_in_filtr()
+    desktop_sportshop.choice_whey_protein_in_filter()
     desktop_sportshop.sort_by_high_rating()
     desktop_sportshop.purchase_selected_product()
     desktop_sportshop.purchase_order()
